a46828bf678aab34852fb09261dd6c78b4a4a2dd
Filip Kastl <mail@f-k.cz>
2023-11-01 18:01:37 +0100

    Lang switch

--- pages/about.vue
+++ pages/about.vue
@@ -1,5 +1,20 @@
 <template>
-  <div class="test">Test page {{ $t("welcome") }}</div>
+  <div class="wrap">
+    <form>
+      <select v-model="locale">
+        <option value="en">en</option>
+        <option value="fr">fr</option>
+      </select>
+      <p>{{ $t("welcome") }}</p>
+    </form>
 
-  <div class="dalsi">Nic</div>
+    <div class="test">Test page {{ $t("welcome") }}</div>
+
+    <div class="dalsi">Nic</div>
+  </div>
 </template>
+
+<script lang="ts" setup>
+import { useI18n } from "#imports";
+const { locale } = useI18n();
+</script>
