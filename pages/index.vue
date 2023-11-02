3d56d02ad7494df6dfafbb99e3f35fe6d4b00aba
Filip Kastl <mail@f-k.cz>
2023-11-02 13:30:52 +0100

    Tailwind base, logo add

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,13 @@
 <template>
-  <div class="main">
-    MAIN
-
+  <div class="page">
     <Temperature />
+
+    <div class="wrap-test">Test</div>
   </div>
 </template>
+
+<style lang="postcss">
+.wrap-test {
+  @apply bg-blue text-gray-dark p-2 m-5;
+}
+</style>
